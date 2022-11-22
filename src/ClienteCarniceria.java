a61e79e18d8f6b9b52ef04f7fd7e5187c372c728
mdevos <martadevos@iesnervion.es>
2022-11-22 14:13:32 +0100

    Initial commit

---
+++ src/ClienteCarniceria.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.Semaphore;
+
+public class ClienteCarniceria implements Runnable{
+
+    Semaphore semaforo = new Semaphore(4);
+
+    @Override
+    public void run() {
+        try {
+            semaforo.acquire();
+            System.out.println("El "+ Thread.currentThread().getName()+" está siendo atendido");
+            Thread.sleep((long) (1000*(Math.random()*10+3)));
+            System.out.println("El "+ Thread.currentThread().getName()+" ha terminado en la carniceria");
+            semaforo.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ClienteCarniceria carniceria = new ClienteCarniceria();
+
+        for(int i = 1; i<11; i++){
+            Thread hilo = new Thread(carniceria);
+            hilo.setName("cliente "+ i);
+            hilo.start();
+        }
+    }
+}
